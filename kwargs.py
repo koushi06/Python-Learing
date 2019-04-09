93f344a9e9c6a8b7c5c706f78ec45e9528ee7940
koushi06 <kousalya315@gmail.com>
2019-04-09 08:04:22 -0400

    variable argument kwargs

---
+++ kwargs.py
@@ -0,0 +1,9 @@
+
+def details(**file):
+
+    for key,value in file.items():
+
+        print("{} is {}".format(key,value))
+
+details(name="hari",age=24,city="hyderabad",Ofce="CTS")
+details(name="muni",age=21,city="Banglore",clg="SVS")
